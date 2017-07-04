e5157a192f37c0a92abef1cd64478d165342fe97
HEMAVARTHINI MARAPPAN <bratmutter2014@gmail.com>
2017-07-04 18:31:00 +0530

    Update Alphabet.java

--- Alphabet.java
+++ Alphabet.java
@@ -1,22 +1,14 @@
-import java.util.Scanner;
-
-public class Alphabet
+import java.io.*;
+public class Alpha
 {
-    public static void main(String args[])
-    {
-        char ch;
-        Scanner scan = new Scanner(System.in);
-		
-        System.out.print("Enter a Character : ");
-        ch = scan.next().charAt(0);
-		
-        if((ch>='a' && ch<='z') || (ch>='A' && ch<='Z'))
-        {
-            System.out.print(ch + " is an Alphabet");
-        }
-        else
-        {
-            System.out.print(ch + " is not an Alphabet");
-        }
-    }
+	public static void main(String args[])throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamRedaer(System.in));
+		String str = br.readLine();
+		if((str>=a && str<=z) || (str>=A && str<=Z))
+			System.out.println("Alphabet");
+		else
+			System.out.println("Not an Alphabet");
+	}
+	
 }
