443888b5a9e01d979cbd128769d574f58ea444a1
HEMAVARTHINI MARAPPAN <bratmutter2014@gmail.com>
2017-07-05 20:59:01 +0530

    Create CamelCase.java

---
+++ CamelCase.java
@@ -0,0 +1,26 @@
+import java.io.*;
+public class CamelCase
+{
+public static void main(String args[])throws IOException
+{
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+String str = br.readLine();
+String z = "";
+Character c = str.charAt(0);
+z = z+Character.toUpperCase(c);
+for(int i=0;i<str.length();i++)
+{
+Character c1 = str.charAt(i);
+Character c2 = str.charAt(i-1);
+if(c2 == ' ')
+{
+z = z + Character.toUpperCase(c1);
+}
+else
+{
+z = z + c1;
+}
+}
+System.out.println(z);
+}
+}
