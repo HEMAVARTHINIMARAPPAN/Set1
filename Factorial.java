e7012159924a2f269bd8a56e8175c1985a1e6c0e
HEMAVARTHINI MARAPPAN <bratmutter2014@gmail.com>
2017-07-05 10:42:20 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,14 @@
+import java.io.*;
+public class Factorial
+{
+public static void main(String args[])throws IOException
+{
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+Integer x = Integer.parseInt(br.readLine());
+Integer fact = 1;
+for (int i=1;i<=x;i++)
+{
+fact = fact*i;
+}
+}
+}
