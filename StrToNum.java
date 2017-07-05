c0dec1747f5008f8533e78afd5d39ea042ee1a37
HEMAVARTHINI MARAPPAN <bratmutter2014@gmail.com>
2017-07-05 12:05:22 +0530

    Update StrToNum.java

--- StrToNum.java
+++ StrToNum.java
@@ -3,10 +3,27 @@ public class StrToNum
 {
 	public static void main(String args[])throws IOException
 	{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String x = br.readLine();
-		Integer n = Integer.parseInt(x);
-		System.out.println(n+3);
+		try
+		{
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			String z = "";
+			String x = br.readLine();
+			char c[] = x.toCharArray();
+			for(int i=0;i<c.length;i++)
+			{
+				if((c[i] == '0' ) || (c[i] == '1' ) || (c[i] == '2' ) || (c[i] == '3' ) || (c[i] == '4' ) || (c[i] == '5' ) || (c[i] == '6' ) || (c[i] == '7' ) || (c[i] == '8' ) || (c[i] == '9' ))
+				{
+					z = z+c[i];
+				}
+			}
+		
+			Integer n = Integer.parseInt(z);
+			System.out.println(n);
+		}
+		catch(Exception e)
+		{
+			System.out.println("NOOO");
+		}
 		
 		
 	}
