c03c41490c8075331c297a0629e3af6ad15ee65f
HEMAVARTHINI MARAPPAN <bratmutter2014@gmail.com>
2017-07-06 22:25:20 +0530

    Update CountPrime.java

--- CountPrime.java
+++ CountPrime.java
@@ -1,23 +1,27 @@
 import java.io.*;
 public class CountPrime
 {
-  public static void main(String[] args) throws IOException
-  {
-    BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-    System.out.print("\nEnter the range : ");
-    int range=Integer.parseInt(br.readLine());
-    for(int i=2;i<=range;i++)
+    public static void main(String[] args) throws IOException
     {
-      int flag=0;
-      for(int j=2;j<i;j++)
-      {
-        if(i%j==0)
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        
+System.out.print("\nEnter the range : ");
+        int range=Integer.parseInt(br.readLine());
+int count =0;
+        for(int i=2;i<=range;i++)
         {
-          flag=1;
-        }
-      }
-      if(flag==0)
-        System.out.print(i+" ");
-    }
+
+            int flag=0;
+            for(int j=2;j<i;j++)
+            {
+                if(i%j==0)
+                {
+                    flag = 1;
+                }
+            }
+            if(flag==0)
+            count++;
+          }
+System.out.print(count);
 }
 }
