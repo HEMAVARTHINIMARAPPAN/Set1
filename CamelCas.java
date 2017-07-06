0cb8a0d5181b7abfc02b17eb024c5fa9dcb85213
HEMAVARTHINI MARAPPAN <bratmutter2014@gmail.com>
2017-07-06 21:36:26 +0530

    Update and rename CamelCase.java to CamelCas.java

---
+++ CamelCas.java
@@ -0,0 +1,27 @@
+import java.io.*;
+public class CamelCas
+{
+public static void main(String args[])throws IOException
+{
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+System.out.println("Enter a sentence");
+String str = br.readLine();
+String z = "";
+Character c = str.charAt(0);
+z = z+Character.toUpperCase(c);
+for(int i=1;i<str.length();i++)
+{
+Character c1 = str.charAt(i);
+Character c2 = str.charAt(i-1);
+if(c2 == ' ')
+{
+z = z + Character.toUpperCase(c1);
+}
+else
+{
+z = z + c1;
+}
+}
+System.out.println(z);
+}
+}
