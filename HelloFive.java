47ea9c53f7b8ff61461a76f62c6174dbab1a0483
HEMAVARTHINI MARAPPAN <bratmutter2014@gmail.com>
2017-07-07 21:05:21 +0530

    Create HelloFive.java

---
+++ HelloFive.java
@@ -0,0 +1,13 @@
+import java.io.*;
+public class HelloFive
+{
+public static void main(String args[])throws IOException
+{
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+String s = "Hello";
+for(int i=0;i<5;i++)
+{
+System.out.println("Hello");
+}
+}
+}
