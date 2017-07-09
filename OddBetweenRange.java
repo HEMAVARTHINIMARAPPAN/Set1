22a482a07bb59b9d111676f1aa30df55e6f652db
HEMAVARTHINI MARAPPAN <bratmutter2014@gmail.com>
2017-07-09 11:54:42 +0530

    Create OddBetweenRange.java

---
+++ OddBetweenRange.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class OddBetweenRange{
+public static void main(String args[])throws IOException{
+BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+System.out.println("Enter");
+int x = Integer.parseInt(br.readLine());
+int y = Integer.parseInt(br.readLine());
+int count =0;
+for(int i=x;i<=y;i++)
+{
+if(i%2 != 0)
+count++;
+}
+System.out.println(count);
+}
+}
