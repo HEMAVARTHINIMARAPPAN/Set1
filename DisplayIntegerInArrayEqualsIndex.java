23a76233aa670c642c37d4e42ed814a62892e331
HEMAVARTHINI MARAPPAN <bratmutter2014@gmail.com>
2017-07-11 21:06:24 +0530

    Update and rename IntegerInArrayEqualsIndex.java to DisplayIntegerInArrayEqualsIndex.java

--- DisplayIntegerInArrayEqualsIndex.java
+++ DisplayIntegerInArrayEqualsIndex.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.*;
-public class IntegerInArrayEqualsIndex {
+public class DisplayIntegerInArrayEqualsIndex {
 	public static void main(String args[])throws IOException
 	{
 	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
