160ce12a01b0f90d19ac74c98b5d7126d043c48e
HEMAVARTHINI MARAPPAN <bratmutter2014@gmail.com>
2017-07-11 20:59:37 +0530

    Create new file

---
+++ IntegerInArrayEqualsIndex.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+public class IntegerInArrayEqualsIndex {
+	public static void main(String args[])throws IOException
+	{
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	Integer i,n,a,index;
+	n = Integer.parseInt(br.readLine());
+	ArrayList<Integer> list = new ArrayList<Integer>();
+	for(i=0;i<n;i++)
+	{
+		a = Integer.parseInt(br.readLine());
+		list.add(a);
+		Collections.sort(list);
+	}
+	System.out.println(list);
+	System.out.println("Enter the Index");
+	index = Integer.parseInt(br.readLine());
+	if(index>=n)
+		System.out.println("No Element");
+	else
+	System.out.println(list.get(index));
+	
+
+
+	
+	}
+
+}
