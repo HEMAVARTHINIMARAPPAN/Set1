8962a7fb503a37fe7f3d51e7de092bf8e07f8f9f
HEMAVARTHINI MARAPPAN <bratmutter2014@gmail.com>
2017-12-12 13:00:29 +0530

    Update PrimeBetweenRange.java

--- PrimeBetweenRange.java
+++ PrimeBetweenRange.java
@@ -1,25 +1,29 @@
 import java.io.*;
 public class PrimeBetweenRange{
 public static void main(String args[])throws IOException{
-BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-String  primeNumbers = "";
-Integer x = Integer.parseInt(br.readLine());
-Integer y = Integer.parseInt(br.readLine());
-for (int i = x; i <= y; i++)         
-{
-int count=0; 	  
-for(int n =i; n>=x; n--)
-{
-if(i%n==0)
-{
-count++;
-}
-}
-if (count == 2)
-{
-primeNumbers = primeNumbers + i + " ";
-}	
-}	
-System.out.println(primeNumbers);
-}
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String  primeNumbers = "";
+		Integer x = Integer.parseInt(br.readLine());
+		Integer y = Integer.parseInt(br.readLine());
+		int flag=0;
+		for(int i=x;i<=y;i++)
+		{
+			for(int j=2;j<i;j++)
+			{
+				if(i%j ==0)
+				{
+					flag =0;
+					break;
+				}
+				else
+				{
+					flag=1;
+				}
+			}
+			if(flag==1)
+			{
+				System.out.println(i);
+			}
+		}
+	}
 }
